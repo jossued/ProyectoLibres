b1d165c67220ad21fd0e129e4b3c09173c079d0a
jossued <jossuedutan@gmail.com>
2018-07-08 17:19:10 -0500

    arreglado visualizar en admin

--- sgoa/aplicacion/modulos_administrador/adm_comentarios.php
+++ sgoa/aplicacion/modulos_administrador/adm_comentarios.php
@@ -48,9 +48,7 @@ if (@!$_SESSION['usuario']) {
     }
 
     /* Set height of the grid so .sidenav can be 100% (adjust as needed) */
-    .row.content {
-        height: 390px
-    }
+    .row.content {height: 390px}
 
     /* Set gray background color and 100% height */
     .sidenav {
@@ -60,16 +58,14 @@ if (@!$_SESSION['usuario']) {
     }
 
     /* Set black background color, white text and some padding */
-    html {
+    html{
         min-height: 100%;
         position: relative;
     }
-
-    body {
-        margin: 0;
+    body{
+        margin:0;
         margin-bottom: 40px;
     }
-
     /* Set black background color, white text and some padding */
     footer {
         background-color: #555;
@@ -86,9 +82,95 @@ if (@!$_SESSION['usuario']) {
             height: auto;
             padding: 15px;
         }
+        .row.content {height:auto;}
+    }
 
-        .row.content {
-            height: auto;
+
+    body {font-family: Arial, Helvetica, sans-serif;}
+
+    #myImg {
+        border-radius: 5px;
+        cursor: pointer;
+        transition: 0.3s;
+    }
+
+    #myImg:hover {opacity: 0.7;}
+
+    /* The Modal (background) */
+    .modal {
+        display: none; /* Hidden by default */
+        position: fixed; /* Stay in place */
+        z-index: 1; /* Sit on top */
+        padding-top: 100px; /* Location of the box */
+        left: 0;
+        top: 0;
+        width: 100%; /* Full width */
+        height: 100%; /* Full height */
+        overflow: auto; /* Enable scroll if needed */
+        background-color: rgb(0,0,0); /* Fallback color */
+        background-color: rgba(0,0,0,0.9); /* Black w/ opacity */
+    }
+
+    /* Modal Content (image) */
+    .modal-content {
+        margin: auto;
+        display: block;
+        width: 80%;
+        max-width: 700px;
+    }
+
+    /* Caption of Modal Image */
+    #caption {
+        margin: auto;
+        display: block;
+        width: 80%;
+        max-width: 700px;
+        text-align: center;
+        color: #ccc;
+        padding: 10px 0;
+        height: 150px;
+    }
+
+    /* Add Animation */
+    .modal-content, #caption {
+        -webkit-animation-name: zoom;
+        -webkit-animation-duration: 0.6s;
+        animation-name: zoom;
+        animation-duration: 0.6s;
+    }
+
+    @-webkit-keyframes zoom {
+        from {-webkit-transform:scale(0)}
+        to {-webkit-transform:scale(1)}
+    }
+
+    @keyframes zoom {
+        from {transform:scale(0)}
+        to {transform:scale(1)}
+    }
+
+    /* The Close Button */
+    .close {
+        position: absolute;
+        top: 15px;
+        right: 35px;
+        color: #f1f1f1;
+        font-size: 40px;
+        font-weight: bold;
+        transition: 0.3s;
+    }
+
+    .close:hover,
+    .close:focus {
+        color: #bbb;
+        text-decoration: none;
+        cursor: pointer;
+    }
+
+    /* 100% Image Width on Smaller Screens */
+    @media only screen and (max-width: 700px){
+        .modal-content {
+            width: 100%;
         }
     }
 </style>
@@ -130,6 +212,11 @@ $objeto_de_aprendizaje = obtener_oa_como_arreglo($id_objeto_aprendizaje);
                 </tr>
                 </thead>
                 <tbody>
+                <div id="myModal" class="modal">
+                    <span class="close">&times;</span>
+                    <img class="modal-content" id="img01">
+                    <div id="caption"></div>
+                </div>
                 <?php
                 $statement = "select * from comentario where id_objeto_aprendizaje=?";
                 $conexion = new Conexion();
